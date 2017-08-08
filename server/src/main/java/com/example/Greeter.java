7da8840b70e73a52c24d30f1adc9b0db08eaa701
Raphaël Moreau <rphlmr@users.noreply.github.com>
2017-08-08 09:50:50 +0200

    fix test

--- server/src/main/java/com/example/Greeter.java
+++ server/src/main/java/com/example/Greeter.java
@@ -5,15 +5,13 @@ package com.example;
  */
 public class Greeter {
 
-  /**
-   * This is a constructor.
-   */
+
   public Greeter() {
 
   }
 
 
-  public final String greet(String someone) {
+  public final String greet(final String someone) {
     return String.format("Hello, %s!", someone);
   }
 }
