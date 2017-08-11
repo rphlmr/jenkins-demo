58222c6a244a8aad034335213b9934ea736380b5
Raphaël Moreau <rphlmr@users.noreply.github.com>
2017-08-11 19:27:01 +0200

    Update Greeter.java

--- server/src/main/java/com/example/Greeter.java
+++ server/src/main/java/com/example/Greeter.java
@@ -12,11 +12,7 @@ public class Greeter {
 
   }
 
-   /**
-   * This is a method. Cool.
-   * @param someone the name of a person
-   * @return the sentence
-   */
+
   public final String greet(final String someone) {
     return String.format("Hello, %s!", someone);
   }
